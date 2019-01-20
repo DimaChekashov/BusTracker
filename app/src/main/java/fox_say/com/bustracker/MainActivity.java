2c28c3cd7b34dfb37910079d9a887ae35e827266
DimaChekashov <foxdima99@gmail.com>
2019-01-21 01:35:02 +0300

    Add bottom navigation

--- app/src/main/java/fox_say/com/bustracker/MainActivity.java
+++ app/src/main/java/fox_say/com/bustracker/MainActivity.java
@@ -1,13 +1,47 @@
 package fox_say.com.bustracker;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new TimeFragment()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+
+            switch (item.getItemId()) {
+                case R.id.nav_time:
+                    selectedFragment = new TimeFragment();
+                    break;
+                case R.id.nav_map:
+                    selectedFragment = new MapFragment();
+                    break;
+                case R.id.nav_bus:
+                    selectedFragment = new BusFragment();
+                    break;
+            }
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    selectedFragment).commit();
+
+            return true;
+        }
+    };
 }
