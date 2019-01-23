711b8610be262b787365e7e684de4efdffdbad32
DimaChekashov <foxdima99@gmail.com>
2019-01-23 16:26:22 +0300

    delete MapActivity and resolve problem with init MapKit on Fragment

--- app/src/main/java/fox_say/com/bustracker/MainActivity.java
+++ app/src/main/java/fox_say/com/bustracker/MainActivity.java
@@ -1,6 +1,5 @@
 package fox_say.com.bustracker;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -20,9 +19,6 @@ public class MainActivity extends AppCompatActivity {
 
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                 new TimeFragment()).commit();
-
-        Intent mapIntent = new Intent(this, MapActivity.class);
-        startActivity(mapIntent);
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
--- app/src/main/java/fox_say/com/bustracker/MapFragment.java
+++ app/src/main/java/fox_say/com/bustracker/MapFragment.java
@@ -1,5 +1,6 @@
 package fox_say.com.bustracker;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,7 +9,25 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class MapFragment extends Fragment{
+import com.yandex.mapkit.Animation;
+import com.yandex.mapkit.MapKitFactory;
+import com.yandex.mapkit.geometry.Point;
+import com.yandex.mapkit.map.CameraPosition;
+import com.yandex.mapkit.mapview.MapView;
+
+public class MapFragment extends Fragment {
+
+    private final String MAPKIT_API_KEY = "e2fea7a9-9b0e-437e-b234-1cb35d6f9f5e";
+    private final Point TARGET_LOCATION = new Point(59.945933, 30.320045);
+
+    private MapView mapView;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        MapKitFactory.setApiKey(MAPKIT_API_KEY);
+        MapKitFactory.initialize(context);
+    }
 
     @Nullable
     @Override
@@ -16,4 +35,30 @@ public class MapFragment extends Fragment{
         return inflater.inflate(R.layout.fragment_map, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mapView = (MapView) view.findViewById(R.id.mapView);
+
+        // Перемещение камеры в центр Санкт-Петербурга.
+        mapView.getMap().move(
+                new CameraPosition(TARGET_LOCATION, 14.0f, 0.0f, 0.0f),
+                new Animation(Animation.Type.SMOOTH, 5),
+                null);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        MapKitFactory.getInstance().onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        MapKitFactory.getInstance().onStop();
+        mapView.onStop();
+    }
 }
